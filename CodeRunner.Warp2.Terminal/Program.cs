78fe5ad398b2b7934b8a0d63719cbe5fc5db03e4
Fatih DOĞAN (PeakUp) <fatih.dogan@peakup.org>
2018-09-14 10:45:53 +0300

    Add project files.

---
+++ CodeRunner.Warp2.Terminal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeRunner.Warp2.Terminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
