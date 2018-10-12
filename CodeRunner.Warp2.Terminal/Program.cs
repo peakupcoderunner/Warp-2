724d02b23a085f093945d08a83cd5eda28bf415e
Fatih DOĞAN (PeakUp) <fatih.dogan@peakup.org>
2018-10-12 22:14:35 +0300

    Initial create.

--- CodeRunner.Warp2.Terminal/Program.cs
+++ CodeRunner.Warp2.Terminal/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Amazon.IdentityManagement;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace CodeRunner.Warp2.Terminal
 {
@@ -6,7 +10,22 @@ namespace CodeRunner.Warp2.Terminal
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MainAsync(args).Wait();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            var options = config.GetAWSOptions();
+            var client = options.CreateServiceClient<IAmazonIdentityManagementService>() as AmazonIdentityManagementServiceClient;
+            var response = await client.GetUserAsync();
+            var user = response.User;
+            Console.WriteLine("Hello {0}! The user was created on {1}", user.UserName, user.CreateDate);
+            Console.ReadKey(true);
         }
     }
 }
